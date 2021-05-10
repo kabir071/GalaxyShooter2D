f939b43860efba0a4bdd98b065fb644fdba5a405
kabir071 <wadhwa.kabir07@gmail.com>
2021-05-10 22:55:33 +0530

    Fixed bugs

--- first2dgame/Assets/Scripts/powerup.cs
+++ first2dgame/Assets/Scripts/powerup.cs
@@ -7,23 +7,21 @@ public class powerup : MonoBehaviour
     [SerializeField]
     private float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0,1,0)* speed * Time.deltaTime);
+        transform.Translate(new Vector3(0,1,0) * speed * Time.deltaTime);
 
-        public void OnTriggerEnter2D(Collider2D other)
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
         {
-            if(other.tag="Player")
+            if(other.tag == "Player")
             {
                 Destroy(this.gameObject);
             }
         }
-    }
 }
