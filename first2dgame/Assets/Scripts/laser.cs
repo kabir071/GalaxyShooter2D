5734fa852f867a0b220c44b9c09c34e54c0f0ee8
kabir071 <wadhwa.kabir07@gmail.com>
2021-05-10 22:40:38 +0530

    Finished Movement and SHooting

---
+++ first2dgame/Assets/Scripts/laser.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+       Movement(); 
+    }
+
+    void Movement()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if(transform.position.y > 5.76f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ first2dgame/Assets/Scripts/player1.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    public GameObject laser_prefab;
+    private float canfire = 0.0f;
+    [SerializeField]
+    private float firerate = 0.25f;
+    public bool canTripleShot;
+
+    
+    void Start()
+    {
+        transform.position = new Vector3(0,-3,0);
+    }
+
+    
+    void Update()
+    {
+        Movement();
+        Shooting();
+        
+    }
+
+    void Movement()
+    {
+        //user input
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        
+        //movement
+        transform.Translate(new Vector3(horiz,vert,0) * speed * Time.deltaTime);
+
+        //yaxis boundary
+        if(transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x , 0, 0);
+        }
+
+        if(transform.position.y < -3.71f)
+        {
+            transform.position = new Vector3(transform.position.x , -3.71f, 0);
+        }
+
+        //xaxis boundary
+        if(transform.position.x < -9.891702f)
+        {
+            transform.position = new Vector3(9.587938f ,transform.position.y , 0);
+        }
+        
+        if(transform.position.x > 9.863181f)
+        {
+            transform.position = new Vector3(-9.55f ,transform.position.y , 0);
+        }
+    }
+
+
+    void Shooting()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+           //cooldown system for shooting
+           if(Time.time > canfire)
+           {
+               
+                if(canTripleShot == true)
+                {
+                      Instantiate(laser_prefab, transform.position + new Vector3(-0.3f,1.9f,0), Quaternion.identity);
+                      Instantiate(laser_prefab, transform.position + new Vector3(0.3f,1.9f,0), Quaternion.identity);
+                      Instantiate(laser_prefab, transform.position + new Vector3(0,1.53f,0), Quaternion.identity);
+                      canfire = Time.time + firerate;
+                      
+                }
+                else
+                {
+                    Instantiate(laser_prefab, transform.position + new Vector3(0,1.53f,0), Quaternion.identity);
+                    canfire = Time.time + firerate;
+                }
+               
+           }
+           
+        }
+    }
+
+
+        
+        
+    
+}
